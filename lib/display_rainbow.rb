66f708b7cd4c16a5932e985bd81a3f714af27348
Priscilla H <priscillamhuynh@gmail.com>
2018-06-05 08:06:25 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
-def display_rainbow(color)
-  puts "R: #{color[0]}, Y: #{color[1]}, 
-end
 color="red","orange","yellow","green","blue","indigo","violet"
+def display_rainbow(color)
+  puts "R: #{color[0]}, O: #{color[1]},Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
+end
+
